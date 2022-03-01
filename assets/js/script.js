4bfba17671b44567df1ca7048341a5ea1a49291e
coleleg <coleleg@gmail.com>
2022-03-01 17:11:31 -0500

    add local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,26 +26,28 @@ var day4MinEl = document.querySelector("#day4min");
 var day5MinEl = document.querySelector("#day5min");
 
 // Dates
+let today = new Date().toISOString().slice(0, 10);
+document.getElementById("1card").textContent = today;
+
 let tomorrow = new Date();
 tomorrow.setDate(new Date().getDate() + 1);
-document.getElementById("1card").textContent = tomorrow.toISOString().slice(0, 10);
+document.getElementById("2card").textContent = tomorrow.toISOString().slice(0, 10);
 
 let twoDays = new Date();
 twoDays.setDate(new Date().getDate() + 2);
-document.getElementById("2card").textContent = twoDays.toISOString().slice(0, 10);
+document.getElementById("3card").textContent = twoDays.toISOString().slice(0, 10);
 
 let threeDays = new Date();
 threeDays.setDate(new Date().getDate() + 3);
-document.getElementById("3card").textContent = threeDays.toISOString().slice(0, 10);
+document.getElementById("4card").textContent = threeDays.toISOString().slice(0, 10);
 
 let fourDays = new Date();
 fourDays.setDate(new Date().getDate() + 4);
-document.getElementById("4card").textContent = fourDays.toISOString().slice(0, 10);
+document.getElementById("5card").textContent = fourDays.toISOString().slice(0, 10);
 
-
-console.log(tomorrow.toISOString().slice(0, 10));
-
-
+// Array to hold search Data
+let cities = [];
+let myStorage = window.localStorage;
 
 
 // Deals with submit and submit errors
@@ -57,7 +59,14 @@ function formSubmitHandler (event) {
 
     if (cityName) {
         getCityMain(cityName);
-        
+       
+        // Creates an object for searched cities, pushes them to the cities array and saves them to local storage.
+        let cityDataObj = {
+            name: cityName
+        }
+        cities.push(cityDataObj);
+        saveSearches();
+        loadSearches();
         
     }
         else {
@@ -65,8 +74,6 @@ function formSubmitHandler (event) {
         }
       
 }
-
-
 
 // creates the API URL for submitted city
 function getCityMain (city) {
@@ -91,6 +98,7 @@ function getCityMain (city) {
 
                 let today = new Date().toISOString().slice(0, 10);
                 document.querySelector("#city-header").textContent = city.charAt(0).toUpperCase()  + city.slice(1) + "  " + today;
+                document.getElementById("1card").textContent = today;
               
     
             })
@@ -106,118 +114,153 @@ function getCityMain (city) {
     )};
 
     // takes Latitude and Longitude from the city response (which does not return uvi data) and fetches data from a url that does
-    function getUVIndex (lon, lat) {
-        var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,daily&appid=dee89b7fccd367887e9d18c1350058ee";
+function getUVIndex (lon, lat) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,daily&appid=dee89b7fccd367887e9d18c1350058ee";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                let uvi = data.current.uvi;
+                console.log(uvi);
+                uvEl.textContent = "UV Index: " + uvi;
+
+                // Changes color of uvEl depending on how "dangerous" the uvi is
+                if (uvi < 3) {
+                    uvEl.setAttribute("class", "p-2 border col-2 mx-3 mb-3 bg-success");
+                } else if ( uvi > 2.99 && uvi < 7) {
+                    uvEl.setAttribute("class", "p-2 border col-2 mx-3 mb-3 bg-warning");    
+                } else if (uvi > 7) {
+                    uvEl.setAttribute("class", "p-2 border col-2 mx-3 mb-3 bg-danger");   
+                }
+
+            })
+        }
+    })
     
-        fetch(apiUrl).then(function(response) {
-            if (response.ok) {
-                response.json().then(function(data) {
-                    let uvi = data.current.uvi;
-                    console.log(uvi);
-                    uvEl.textContent = "UV Index: " + uvi;
+};
 
-                    if (uvi < 3) {
-                        uvEl.setAttribute("class", "p-2 border col-2 mx-3 mb-3 bg-success");
-                    } else if ( uvi > 2.99 && uvi < 7) {
-                        uvEl.setAttribute("class", "p-2 border col-2 mx-3 mb-3 bg-warning");    
-                    } else if (uvi > 7) {
-                        uvEl.setAttribute("class", "p-2 border col-2 mx-3 mb-3 bg-danger");   
-                    }
-    
-                })
-            }
-        })
-        
-    };
+function fiveDay (lon, lat) {
+    let apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=Minutely,hourly&units=imperial&appid=dee89b7fccd367887e9d18c1350058ee"
 
-    function fiveDay (lon, lat) {
-        let apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=Minutely,hourly&units=imperial&appid=dee89b7fccd367887e9d18c1350058ee"
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            console.log(response);
+            response.json().then(function(data) {
+                // daily temperature holders
+                let day1Icon;
+                let day2Icon;
+                let day3Icon;
+                let day4Icon;
+                let day5Icon;
+                let day1Max;
+                let day2Max;
+                let day3Max;
+                let day4Max;
+                let day5Max;
+                let day1Min;
+                let day2Min;
+                let day3Min;
+                let day4Min;
+                let day5Min;
 
-        fetch(apiUrl).then(function(response) {
-            if (response.ok) {
-                console.log(response);
-                response.json().then(function(data) {
-                    // daily temperature holders
-                    let day1Icon;
-                    let day2Icon;
-                    let day3Icon;
-                    let day4Icon;
-                    let day5Icon;
-                    let day1Max;
-                    let day2Max;
-                    let day3Max;
-                    let day4Max;
-                    let day5Max;
-                    let day1Min;
-                    let day2Min;
-                    let day3Min;
-                    let day4Min;
-                    let day5Min;
+                // data.daily is an array of JSON data - each day is an index
+                let days = data.daily;
 
-                    // data.daily is an array of JSON data - each day is an index
-                    let days = data.daily;
+                for (i=0; i < days.length; i++) {
 
-                    for (i=0; i < days.length; i++) {
-
-                        day1Icon = [days[0].weather[0].icon];
-                        day2Icon = [days[1].weather[0].icon];
-                        day3Icon = [days[2].weather[0].icon];
-                        day4Icon = [days[3].weather[0].icon];
-                        day5Icon = [days[4].weather[0].icon];
+                    day1Icon = [days[0].weather[0].icon];
+                    day2Icon = [days[1].weather[0].icon];
+                    day3Icon = [days[2].weather[0].icon];
+                    day4Icon = [days[3].weather[0].icon];
+                    day5Icon = [days[4].weather[0].icon];
 
 
-                        day1Max = [days[0].temp.max];
-                        day2Max = [days[1].temp.max];
-                        day3Max = [days[2].temp.max];
-                        day4Max = [days[3].temp.max];
-                        day5Max = [days[4].temp.max];
+                    day1Max = [days[0].temp.max];
+                    day2Max = [days[1].temp.max];
+                    day3Max = [days[2].temp.max];
+                    day4Max = [days[3].temp.max];
+                    day5Max = [days[4].temp.max];
 
-                        day1Min = [days[0].temp.min];
-                        day2Min = [days[1].temp.min];
-                        day3Min = [days[2].temp.min];
-                        day4Min = [days[3].temp.min];
-                        day5Min = [days[4].temp.min];
-                    };
+                    day1Min = [days[0].temp.min];
+                    day2Min = [days[1].temp.min];
+                    day3Min = [days[2].temp.min];
+                    day4Min = [days[3].temp.min];
+                    day5Min = [days[4].temp.min];
+                };
+            
+                // Five Day and Current Weather Icons
+                let day1IconUrl =  "http://openweathermap.org/img/w/" + day1Icon + ".png";
+                weatherIconEl.setAttribute('src', day1IconUrl);
+                document.querySelector("#weather-1").setAttribute('src', day1IconUrl);
+
+                let day2IconUrl =  "http://openweathermap.org/img/w/" + day2Icon + ".png";
+                document.querySelector("#weather-2").setAttribute('src', day2IconUrl);
+
+                let day3IconUrl =  "http://openweathermap.org/img/w/" + day3Icon + ".png";
+                document.querySelector("#weather-3").setAttribute('src', day3IconUrl);
+
+                let day4IconUrl =  "http://openweathermap.org/img/w/" + day4Icon + ".png";
+                document.querySelector("#weather-4").setAttribute('src', day4IconUrl);
+
+                let day5IconUrl =  "http://openweathermap.org/img/w/" + day5Icon + ".png";
+                document.querySelector("#weather-5").setAttribute('src', day5IconUrl);
+
                 
-                    // Five Day and Current Weather Icons
-                    let day1IconUrl =  "http://openweathermap.org/img/w/" + day1Icon + ".png";
-                    weatherIconEl.setAttribute('src', day1IconUrl);
-                    document.querySelector("#weather-1").setAttribute('src', day1IconUrl);
+                // Five Day Highs and Lows
+                day1MaxEl.textContent = "High: " + day1Max;
+                day1MinEl.textContent = "Low: " + day1Min;
 
-                    let day2IconUrl =  "http://openweathermap.org/img/w/" + day2Icon + ".png";
-                    document.querySelector("#weather-2").setAttribute('src', day2IconUrl);
+                day2MaxEl.textContent = "High: " + day2Max;
+                day2MinEl.textContent = "Low: " + day2Min;
 
-                    let day3IconUrl =  "http://openweathermap.org/img/w/" + day3Icon + ".png";
-                    document.querySelector("#weather-3").setAttribute('src', day3IconUrl);
+                day3MaxEl.textContent = "High: " + day3Max;
+                day3MinEl.textContent = "Low: " + day3Min;
 
-                    let day4IconUrl =  "http://openweathermap.org/img/w/" + day4Icon + ".png";
-                    document.querySelector("#weather-4").setAttribute('src', day4IconUrl);
+                day4MaxEl.textContent = "High: " + day4Max;
+                day4MinEl.textContent = "Low: " + day4Min;
 
-                    let day5IconUrl =  "http://openweathermap.org/img/w/" + day5Icon + ".png";
-                    document.querySelector("#weather-5").setAttribute('src', day5IconUrl);
+                day5MaxEl.textContent = "High: " + day5Max;
+                day5MinEl.textContent = "Low: " + day5Min;
+            })
+        }
+    })
+};
 
-                    
-                    // Five Day Highs and Lows
-                    day1MaxEl.textContent = "High: " + day1Max;
-                    day1MinEl.textContent = "Low: " + day1Min;
+// Creates a list of saved searches
+function createSavedSearchEl(city) {
+    const listEl = document.querySelector("#saved-searches");
+    
+    let listItemEl = document.createElement('li');
+    listItemEl.className = 'p-2 bg-light border col-2 mx-3';
+    listItemEl.textContent = city;
 
-                    day2MaxEl.textContent = "High: " + day2Max;
-                    day2MinEl.textContent = "Low: " + day2Min;
+    listEl.appendChild(listItemEl);
 
-                    day3MaxEl.textContent = "High: " + day3Max;
-                    day3MinEl.textContent = "Low: " + day3Min;
+    saveSearches();
 
-                    day4MaxEl.textContent = "High: " + day4Max;
-                    day4MinEl.textContent = "Low: " + day4Min;
+};
 
-                    day5MaxEl.textContent = "High: " + day5Max;
-                    day5MinEl.textContent = "Low: " + day5Min;
-                })
-            }
-        })
+// Saves searches to local 
+function saveSearches() {
+    myStorage.setItem('cities', JSON.stringify(cities));
+};
+
+// Loads saved searches
+function loadSearches() {
+    let savedSearches = myStorage.getItem('cities');
+    if (!savedSearches) {
+        return false;
     }
 
+    savedSearches = JSON.parse(savedSearches);
+
+    for (i=0; i < savedSearches.length; i++) {
+        createSavedSearchEl(savedSearches[i]);
+    }
+};
 
 
 
 userFormEl.addEventListener("click", formSubmitHandler);
+
+loadSearches();
