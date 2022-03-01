8c2261bf79ae6d56c9dae751d35890591a072547
coleleg <coleleg@gmail.com>
2022-02-28 20:17:37 -0500

    add html elements for 5-day forecast and displays real api data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,29 @@
 // this is the api url: api.openweathermap.org/data/2.5/weather?q=London&appid={API key}
 
+// User Form Elements
 var userFormEl = document.querySelector("#submit-btn");
 var cityNameEl = document.querySelector("#city-input");
+
+// Current Weather Elements
 var tempEl = document.querySelector("#temp");
 var windEl = document.querySelector("#wind");
 var humidityEl = document.querySelector("#humidity");
 var uvEl = document.querySelector("#uv");
 
-var lon = "";
-var lat = "";
+// Five Day High Elements
+var day1MaxEl = document.querySelector("#day1max");
+var day2MaxEl = document.querySelector("#day2max");
+var day3MaxEl = document.querySelector("#day3max");
+var day4MaxEl = document.querySelector("#day4max");
+var day5MaxEl = document.querySelector("#day5max");
+
+// Five Day Low Elements
+var day1MinEl = document.querySelector("#day1min");
+var day2MinEl = document.querySelector("#day2min");
+var day3MinEl = document.querySelector("#day3min");
+var day4MinEl = document.querySelector("#day4min");
+var day5MinEl = document.querySelector("#day5min");
+
 
 // Deals with submit and submit errors
 function formSubmitHandler (event) {
@@ -19,9 +34,6 @@ function formSubmitHandler (event) {
 
     if (cityName) {
         getCityMain(cityName)
-        console.log(lon);
-        console.log(lat);
-        getUVIndex(lon, lat);
         
     }
         else {
@@ -88,38 +100,56 @@ function getCityMain (city) {
     };
 
     function fiveDay (lon, lat) {
-        let apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial&appid=dee89b7fccd367887e9d18c1350058ee"
+        let apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=Minutely,hourly&units=imperial&appid=dee89b7fccd367887e9d18c1350058ee"
 
         fetch(apiUrl).then(function(response) {
             if (response.ok) {
                 console.log(response);
                 response.json().then(function(data) {
-                    let day1max;
-                    let day2max;
-                    let day3max;
-                    let day4max;
-                    let day5max;
-                    let day1min;
-                    let day2min;
-                    let day3min;
-                    let day4min;
-                    let day5min;
-                    let days = data.daily;
-                    for (i=0; i < days.length; i++) {
-                        day1max = [days[0].temp.max];
-                        day2max = [days[1].temp.max];
-                        day3max = [days[2].temp.max];
-                        day4max = [days[3].temp.max];
-                        day5max = [days[4].temp.max];
+                    // daily temperature holders
+                    let day1Max;
+                    let day2Max;
+                    let day3Max;
+                    let day4Max;
+                    let day5Max;
+                    let day1Min;
+                    let day2Min;
+                    let day3Min;
+                    let day4Min;
+                    let day5Min;
 
-                        day1min = [days[0].temp.min];
-                        day2min = [days[1].temp.min];
-                        day3min = [days[2].temp.min];
-                        day4min = [days[3].temp.min];
-                        day5min = [days[4].temp.min];
+                    // data.daily is an array of JSON data - each day is an index
+                    let days = data.daily;
+
+                    for (i=0; i < days.length; i++) {
+                        day1Max = [days[0].temp.max];
+                        day2Max = [days[1].temp.max];
+                        day3Max = [days[2].temp.max];
+                        day4Max = [days[3].temp.max];
+                        day5Max = [days[4].temp.max];
+
+                        day1Min = [days[0].temp.min];
+                        day2Min = [days[1].temp.min];
+                        day3Min = [days[2].temp.min];
+                        day4Min = [days[3].temp.min];
+                        day5Min = [days[4].temp.min];
                     }
 
-                    console.log(day1max, day1min, day2max, day2min);
+                    console.log(day1Max, day1Min, day2Max, day2Min);
+                    day1MaxEl.textContent = "High: " + day1Max;
+                    day1MinEl.textContent = "Low: " + day1Min;
+
+                    day2MaxEl.textContent = "High: " + day2Max;
+                    day2MinEl.textContent = "Low: " + day2Min;
+
+                    day3MaxEl.textContent = "High: " + day3Max;
+                    day3MinEl.textContent = "Low: " + day3Min;
+
+                    day4MaxEl.textContent = "High: " + day4Max;
+                    day4MinEl.textContent = "Low: " + day4Min;
+
+                    day5MaxEl.textContent = "High: " + day5Max;
+                    day5MinEl.textContent = "Low: " + day5Min;
                 })
             }
         })
